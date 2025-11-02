fe31e85de032070743828244b35bbd67d0439b0f
Esmond <esmond0717@gmail.com>
2025-11-02 22:55:47 +0800

    輸入模塊的信號功能

--- src/game/scene/game_scene.cpp
+++ src/game/scene/game_scene.cpp
@@ -1,13 +1,38 @@
 #include "game_scene.h"
+#include "../../engine/core/context.h"
+#include "../../engine/input/input_manager.h"
+#include <entt/signal/sigh.hpp>
+#include <spdlog/spdlog.h>
 
-namespace game::scene
-{
-    GameScene::GameScene(engine::core::Context &context, engine::scene::SceneManager &scene_manager)
-        : engine::scene::Scene("GameScene", context, scene_manager)
-    {
-    }
+namespace game::scene {
 
-    GameScene::~GameScene()
-    {
-    }
+GameScene::GameScene(engine::core::Context& context, engine::scene::SceneManager& scene_manager)
+    : engine::scene::Scene("GameScene", context, scene_manager) {
+}
+
+GameScene::~GameScene() {
+}
+
+void GameScene::init() {
+    // 注册输入回调事件 (J,K 键)
+    auto& input_manager = context_.getInputManager();
+    input_manager.onAction("attack").connect<&GameScene::onAttack>(this);       // 默认绑定PRESSED
+    input_manager.onAction("jump", engine::input::ActionState::RELEASED).connect<&GameScene::onJump>(this);
+}
+
+void GameScene::clean() {
+    // 断开输入回调事件 (谁连接，谁负责断开)
+    auto& input_manager = context_.getInputManager();
+    input_manager.onAction("attack").disconnect<&GameScene::onAttack>(this);
+    input_manager.onAction("jump", engine::input::ActionState::RELEASED).disconnect<&GameScene::onJump>(this);
+}
+
+void GameScene::onAttack() {
+    spdlog::info("onAttack");
+}
+
+void GameScene::onJump() {
+    spdlog::info("onJump");
+}
+
 } // namespace game::scene
--- src/game/scene/game_scene.h
+++ src/game/scene/game_scene.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "../../engine/scene/scene.h"
 
-namespace game::scene
-{
-    class GameScene : public engine::scene::Scene
-    {
-    public:
-        GameScene(engine::core::Context &context, engine::scene::SceneManager &scene_manager);
-        ~GameScene();
-    };
+namespace game::scene {
+
+class GameScene final: public engine::scene::Scene {
+public:
+    GameScene(engine::core::Context& context, engine::scene::SceneManager& scene_manager);
+    ~GameScene();
+
+    void init() override;
+    void clean() override;
+
+private:
+    // --- 测试输入回调事件 ---
+    void onAttack();
+    void onJump();
+
+};
+
 } // namespace game::scene
